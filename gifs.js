ee796e1dff78f1a53afeaf99f08ffe849371c5ae
stephorpilla <stephanieorpilla@yahoo.com>
2016-10-28 11:56:18 -0700

    finished styling and fixed gif start stop

--- gifs.js
+++ gifs.js
@@ -1,78 +1,82 @@
-var gifArray = ["panda", "haha", "puppies", "funny", "hug", "surprise", "yay"];
+var gifArray = ["cats", "lima beans", "stars", "game of thrones", "haha", "surprise", "yay", "spooky", "shark", "hipsters", "water", "ugh", "nature", "america", "roller coaster", "frozen"];
 
-function aNewButton(what) {
-	var newButton = $("<button></button>");
-	newButton.addClass("cutegif");
-	newButton.attr("data-name", what);
-	newButton.text(what);
-	$("#buttonsHere").append(newButton);
-}
+$(document).ready(function() {
 
-for (i=0; i<gifArray.length; i++){
-	aNewButton(gifArray[i]);
-}
+	dothatShit();
+	function dothatShit(){
+		for (i=0; i<gifArray.length; i++){
+			aNewButton(gifArray[i]);
+		}
+	}
 
-$("#getGifs").on("submit", function() {
-	var userGif = $("#gifInput").val().trim();
-	console.log(userGif);
-	aNewButton(userGif);
-	return false;
+	function aNewButton(what) {
+		var newButton = $("<button></button>");
+		newButton.addClass("cutegif");
+		newButton.attr("data-name", what);
+		newButton.text(what.toUpperCase());
+		$("#buttonsHere").append(newButton);
+		console.log("I did it all!");
+
+		$(".cutegif").on("click", function(){
+			var gif = $(this).data('name');
+			$("#first").empty();
+			$("#second").empty();
+			$("#third").empty();
+			$("#fourth").empty();
+			var gifNumber = "&limit=100";
+			var apiKey = "&api_key=dc6zaTOxFJmzC&limit=10";
+			var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + gifNumber +apiKey;
+
+			function gifStuff (zero, meow, where) {
+			$.ajax({url: queryURL, method: 'GET'})
+				.done(function(response) {
+		            var gifInfo = response.data;
+
+	            	for (var i = zero; i < meow; i++) {
+		                var gifDiv = $('<div class="helloGifs">')
+		                var actualGif = $('<img>');
+		                var animated = gifInfo[i].images.fixed_height.url;
+             			var still = gifInfo[i].images.fixed_height_still.url;
+             			actualGif.attr('src', animated);
+			            actualGif.attr('data-still', still);
+			            actualGif.attr('data-animated', animated);
+			            actualGif.attr('data-state', 'animated')
+			            actualGif.addClass("imaGIF");
+		                gifDiv.append(actualGif);
+		                where.append(gifDiv);
+		            }
+		        });
+		   	}
+
+			gifStuff(0, 25, $("#first"));
+	        gifStuff(25, 50, $("#second"));
+	        gifStuff(50, 75, $("#third"));
+	        gifStuff(75, 100, $("#fourth"));
+		});
+	}
+
+	$("#getGifs").submit(function() {
+		$("#buttonsHere").empty();
+		var userGif = $("#gifInput").val().trim();
+		gifArray.push(userGif);
+		console.log(gifArray);
+		dothatShit();
+		$("form").trigger("reset");
+		return false;
+
+	});
 });
 
-
-$(".cutegif").click(function(){
-	alert("qjewkrlj");
-	var gif = $(this).data('name');
-	alert("Please be patient as your "+gif+" gifs load.");
-	$("#first").empty();
-	$("#second").empty();
-	$("#third").empty();
-	$("#fourth").empty();
-	var gifNumber = "&limit=100";
-	var apiKey = "&api_key=dc6zaTOxFJmzC&limit=10";
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + gifNumber +apiKey;
-
-	//$("#first").append("<h1>Hi!</h1>");
-	$.ajax({url: queryURL, method: 'GET'})
-		.done(function(response) {
-            var gifInfo = response.data;
-
-
-            function gifStuff (zero, meow, where) {
-            	for (var i = zero; i < meow; i++) {
-	                var gifDiv = $('<div class="helloGifs">');
-	                var rating = gifInfo[i].rating;
-	                //var ratingText = $('<p class="textRating">').text("Rated " + rating);
-	                var actualGif = $('<img>');
-	                actualGif.attr('src', gifInfo[i].images.fixed_height.url);
-
-	                //append the rating and image
-	                //gifDiv.append(ratingText);
-	                gifDiv.append(actualGif);
-
-	                where.append(gifDiv);
-	                // $("#first").append("<h1>Hi!</h1>");
-	            }
-
-            }
-            gifStuff(0, 26, $("#first"));
-            gifStuff(26, 51, $("#second"));
-            gifStuff(51, 76, $("#third"));
-            gifStuff(76, 101, $("#fourth"));
-            // for (var i = 0; i < 26; i++) {
-            //     var gifDiv = $('<div class="helloGifs">');
-            //     var rating = gifInfo[i].rating;
-            //     //var ratingText = $('<p class="textRating">').text("Rated " + rating);
-            //     var actualGif = $('<img>');
-            //     actualGif.attr('src', gifInfo[i].images.fixed_height.url);
-
-            //     //append the rating and image
-            //     //gifDiv.append(ratingText);
-            //     gifDiv.append(actualGif);
-
-            //     $("#first").append(gifDiv);
-            //     // $("#first").append("<h1>Hi!</h1>");
-            // }
-
-        });
+$(document).on('click', '.imaGIF', function(){
+	var state = $(this).data('state');
+	console.log(state);
+    if ( state == 'still') {
+        $(this).data('state', 'animated');
+        $(this).attr('src', $(this).data('animated'));
+        
+    }else if (state == "animated") {
+        $(this).data('state', 'still');
+        $(this).attr('src', $(this).data('still'));
+        
+    }
 });
