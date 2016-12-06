a43db7351a7e9d2e9c9e2c824d10a65f5a9d7665
Stephanie Orpilla <stephanieorpilla@yahoo.com>
2016-12-06 11:27:27 -0800

    fixed duplicate gifs issue

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -19,39 +19,34 @@ $(document).ready(function() {
 
 		$(".cutegif").on("click", function(){
 			var gif = $(this).data('name');
-			$("#first").empty();
-			$("#second").empty();
-			$("#third").empty();
-			$("#fourth").empty();
 			var gifNumber = "&limit=100";
 			var apiKey = "&api_key=dc6zaTOxFJmzC";
 			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + gifNumber +apiKey;
-
-			function gifStuff (zero, meow, where) {
-			$.ajax({url: queryURL, method: 'GET'})
-				.done(function(response) {
-		            var gifInfo = response.data;
-
-	            	for (var i = zero; i < meow; i++) {
-		                var gifDiv = $('<div class="helloGifs">')
-		                var actualGif = $('<img>');
-		                var animated = gifInfo[i].images.fixed_height.url;
-             			var still = gifInfo[i].images.fixed_height_still.url;
-             			actualGif.attr('src', animated);
-			            actualGif.attr('data-still', still);
-			            actualGif.attr('data-animated', animated);
-			            actualGif.attr('data-state', 'animated')
-			            actualGif.addClass("imaGIF");
-		                gifDiv.append(actualGif);
-		                where.append(gifDiv);
-		            }
-		        });
-		   	}
-
 			gifStuff(0, 25, $("#first"));
-	        gifStuff(25, 50, $("#second"));
-	        gifStuff(50, 75, $("#third"));
-	        gifStuff(75, 100, $("#fourth"));
+	        	gifStuff(25, 50, $("#second"));
+	        	gifStuff(50, 75, $("#third"));
+			gifStuff(75, 100, $("#fourth"));
+			
+			function gifStuff (zero, meow, where) {
+				$.ajax({url: queryURL, method: 'GET'})
+				.done(function(response) {
+		           		var gifInfo = response.data;
+                			where.empty();
+					for (var i = zero; i < meow; i++) {
+						var gifDiv = $('<div class="helloGifs">')
+						var actualGif = $('<img>');
+						var animated = gifInfo[i].images.fixed_height.url;
+						var still = gifInfo[i].images.fixed_height_still.url;
+						actualGif.attr('src', animated);
+						    actualGif.attr('data-still', still);
+						    actualGif.attr('data-animated', animated);
+						    actualGif.attr('data-state', 'animated')
+						    actualGif.addClass("imaGIF");
+						gifDiv.append(actualGif);
+						where.append(gifDiv);
+					}
+		       		});
+		   	}
 		});
 	}
 
@@ -63,7 +58,6 @@ $(document).ready(function() {
 		dothatShit();
 		$("form").trigger("reset");
 		return false;
-
 	});
 });
 
@@ -73,10 +67,8 @@ $(document).on('click', '.imaGIF', function(){
     if ( state == 'still') {
         $(this).data('state', 'animated');
         $(this).attr('src', $(this).data('animated'));
-        
     }else if (state == "animated") {
         $(this).data('state', 'still');
         $(this).attr('src', $(this).data('still'));
-        
     }
 });
