b941072742dc5639312e5d77fc5c8ef231033efe
Stephanie Orpilla <stephanieorpilla@yahoo.com>
2016-12-06 11:06:09 -0800

    fixed gif limit parameter

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 			$("#third").empty();
 			$("#fourth").empty();
 			var gifNumber = "&limit=100";
-			var apiKey = "&api_key=dc6zaTOxFJmzC&limit=10";
+			var apiKey = "&api_key=dc6zaTOxFJmzC";
 			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + gifNumber +apiKey;
 
 			function gifStuff (zero, meow, where) {
